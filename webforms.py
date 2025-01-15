d9693e8533f5fc3ac39b4b3e79cb46eaee56fc57
Michael Lyons <git@michael.lyo.nz>
2025-01-14 20:40:00 -0500

    Add command to open front-end from CodeBehind/File

--- webforms.py
+++ webforms.py
@@ -1,5 +1,7 @@
 import sublime
 import sublime_plugin
+from re import search
+from os.path import isfile
 
 
 class OpenCodeFileFromFrontEndCommand(sublime_plugin.TextCommand):
@@ -15,3 +17,19 @@ class OpenCodeFileFromFrontEndCommand(sublime_plugin.TextCommand):
         region = self.view.find_by_selector('meta.path.codefile')[0]
         codefile = self.view.substr(region)
         self.view.window().open_file(str(codefile))
+
+
+class OpenFrontEndFromCodeFileCommand(sublime_plugin.TextCommand):
+    """Naively open foo.ascx from foo.ascx.cs"""
+
+    def is_enabled(self):
+        filename = self.view.file_name()
+        if ((filename
+             and self.view.match_selector(0, 'source.cs')
+             and search(r'\.(?:as[pc]x|master)\.cs$', filename)
+             and isfile(filename[:-3]))):
+            return True
+        return False
+
+    def run(self, edit):
+        self.view.window().open_file(self.view.file_name()[:-3])
